4bc98b62f983daabfb024ce4b589a1ff0a403f05
mik3sw <mikedev.xda@gmail.com>
2020-08-28 17:01:05 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 from setuptools import find_packages, setup
 
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='covid19italy',
     packages=find_packages(include=['covid19italy']),
-    version='1.0.0',
+    version='1.1.0',
     description='Covid-19 italy report',
     author='Mik3sw',
     license='MIT',
